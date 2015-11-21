0b2f408f604fa111151a3f35ff9f8d7c4b2c0943
Kenneth Durbrow <kmdurbrow@me.com>
2015-11-20 20:19:26 -0500

    Added SRA Run List

--- efetch.py
+++ efetch.py
@@ -12,8 +12,10 @@
 """
 
 import httplib
+import string
 import json
 import sys
+import xml.parsers.expat
 
 def _query(function, params):
     if 'db' not in params: params['db'] = 'nuccore' # set default database
@@ -38,6 +40,58 @@ def _lines(resp):
             rslt = buffer[at:next]
             at = next + 1
             yield rslt
+
+
+class SRARun:
+	def _start_element(self, name, attrs):
+		self.properties = attrs;
+		
+	def __init__(self, xmlStr):
+		self.properties = []
+		xmlStr = xmlStr.strip(string.whitespace)
+		if xmlStr.find("&lt;") == 0:
+			xmlStr = '<' + xmlStr[4:]
+		if xmlStr.rfind('&gt;') == len(xmlStr) - 4:
+			xmlStr = xmlStr[:-4] + '>'
+		p = xml.parsers.expat.ParserCreate('UTF-8')
+		p.StartElementHandler = self._start_element
+		p.Parse(xmlStr.encode('utf-8'))
+	
+	def accession(self):
+		return str(self.properties['acc'])
+	
+	def totalSpots(self):
+		return long(self.properties['total_spots'])
+	
+	def totalBases(self):
+		return long(self.properties['total_bases'])
+	
+	def isPublic(self):
+		return self.properties['load_done'] == 'true' and self.properties['is_public'] == 'true'
+	
+	def __repr__(self):
+		return self.properties.__repr__()
+
+	
+def SRARunList(term):
+    """
+        tries to resolve a given search term to gi's
+        then tries to get SRR's for those gi's
+    """
+    conn = httplib.HTTPConnection("eutils.ncbi.nlm.nih.gov")
+    conn.request('GET', _query('esearch', dict(retmode='json', term=term, db='sra')))
+    resp = conn.getresponse()
+    try:
+        idlist = json.load(resp)['esearchresult']['idlist']
+    except:
+    	return []
+    conn.request('GET', _query('esummary', dict(db='sra', retmode='json', id=','.join(idlist))))
+    resp = conn.getresponse()
+    try:
+    	rslt = json.load(resp)['result']
+    	return map((lambda x: SRARun(rslt[x]['runs'])), rslt['uids'])
+    except KeyError:
+    	return None
 
 
 def FASTA(acc):
@@ -71,7 +125,8 @@ def FASTA(acc):
 if __name__ == '__main__':
     import sys
     for arg in sys.argv[1:]:
-        defline, lines = FASTA(arg)
-        print(defline)
-        for line in lines:
-            print(line)
+    	print(map((lambda x: x.totalBases()), SRARunList(arg)))
+#         defline, lines = FASTA(arg)
+#         print(defline)
+#         for line in lines:
+#             print(line)
