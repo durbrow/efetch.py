a38f103e6854a67869b61678bbb76976a1d8be5c
Kenneth Durbrow <kmdurbrow@me.com>
2015-11-20 20:23:31 -0500

    detab

--- efetch.py
+++ efetch.py
@@ -43,36 +43,36 @@ def _lines(resp):
 
 
 class SRARun:
-	def _start_element(self, name, attrs):
-		self.properties = attrs;
-		
-	def __init__(self, xmlStr):
-		self.properties = []
-		xmlStr = xmlStr.strip(string.whitespace)
-		if xmlStr.find("&lt;") == 0:
-			xmlStr = '<' + xmlStr[4:]
-		if xmlStr.rfind('&gt;') == len(xmlStr) - 4:
-			xmlStr = xmlStr[:-4] + '>'
-		p = xml.parsers.expat.ParserCreate('UTF-8')
-		p.StartElementHandler = self._start_element
-		p.Parse(xmlStr.encode('utf-8'))
-	
-	def accession(self):
-		return str(self.properties['acc'])
-	
-	def totalSpots(self):
-		return long(self.properties['total_spots'])
-	
-	def totalBases(self):
-		return long(self.properties['total_bases'])
-	
-	def isPublic(self):
-		return self.properties['load_done'] == 'true' and self.properties['is_public'] == 'true'
-	
-	def __repr__(self):
-		return self.properties.__repr__()
+    def _start_element(self, name, attrs):
+        self.properties = attrs;
+        
+    def __init__(self, xmlStr):
+        self.properties = []
+        xmlStr = xmlStr.strip(string.whitespace)
+        if xmlStr.find("&lt;") == 0:
+            xmlStr = '<' + xmlStr[4:]
+        if xmlStr.rfind('&gt;') == len(xmlStr) - 4:
+            xmlStr = xmlStr[:-4] + '>'
+        p = xml.parsers.expat.ParserCreate('UTF-8')
+        p.StartElementHandler = self._start_element
+        p.Parse(xmlStr.encode('utf-8'))
+    
+    def accession(self):
+        return str(self.properties['acc'])
+    
+    def totalSpots(self):
+        return long(self.properties['total_spots'])
+    
+    def totalBases(self):
+        return long(self.properties['total_bases'])
+    
+    def isPublic(self):
+        return self.properties['load_done'] == 'true' and self.properties['is_public'] == 'true'
+    
+    def __repr__(self):
+        return self.properties.__repr__()
 
-	
+    
 def SRARunList(term):
     """
         tries to resolve a given search term to gi's
@@ -84,14 +84,14 @@ def SRARunList(term):
     try:
         idlist = json.load(resp)['esearchresult']['idlist']
     except:
-    	return []
+        return []
     conn.request('GET', _query('esummary', dict(db='sra', retmode='json', id=','.join(idlist))))
     resp = conn.getresponse()
     try:
-    	rslt = json.load(resp)['result']
-    	return map((lambda x: SRARun(rslt[x]['runs'])), rslt['uids'])
+        rslt = json.load(resp)['result']
+        return map((lambda x: SRARun(rslt[x]['runs'])), rslt['uids'])
     except KeyError:
-    	return None
+        return None
 
 
 def FASTA(acc):
@@ -125,7 +125,7 @@ def FASTA(acc):
 if __name__ == '__main__':
     import sys
     for arg in sys.argv[1:]:
-    	print(map((lambda x: x.totalBases()), SRARunList(arg)))
+        print(map((lambda x: x.totalBases()), SRARunList(arg)))
 #         defline, lines = FASTA(arg)
 #         print(defline)
 #         for line in lines:
